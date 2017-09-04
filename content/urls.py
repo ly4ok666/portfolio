674a36995df034a3a591f78566f542d0d8564ed0
ly4ok <ly4ok666@gmail.com>
2017-09-04 03:52:25 +0300

    Changed the HTML for the site.

---
+++ content/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import url
+from content.views import articles, article, start
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+#from contacts.views import contacts
+#from about_card.views import cards
+
+urlpatterns = [
+    #стартовая страница
+    url(r'^$', start, name='start'),
+    #конкретная статья
+    url(r'^article/get/(?P<article_id>\d+)/$', article, name='статья'),
+    #Все статьи
+    url(r'^articles/$', articles, name='articles'),
+    #Статьи по страницам
+    url(r'^articles/page/(\d+)/$', articles),
+    #Страница с контактами
+#    url(r'^contacts/$', contacts, name='contacts'),
+    #Cтраница с about_card
+#    url(r'^articles/cards/$', cards, name='card'),
+
+     ]
+# url(r'^articles/all/$', articles),
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- content/views.py
+++ content/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+#from django.http.response import HttpResponse
+#from django.template.loader import get_template
+#from django.template import Context
+from django.shortcuts import render_to_response
+from content.models import Article
+#from article.models import Contact
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 # Create your views here.
+
+def start(request):
+    """Домашняя страница нашего приложения Article"""
+    return render(request, 'start.html')
+
+
+
+def articles(request):
+    """Вывод всех статей и пагинация (2 статьи на страницу)"""
+    all_Articles = Article.objects.all()
+    paginator = Paginator(all_Articles, 2)
+    page = request.GET.get('page')
+
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+
+    return render_to_response('articles.html', {'articles': articles })
+
+def article(request, article_id=1):
+    """Вывод одной конкретной статьи """
+    return render_to_response('article.html', {'article': Article.objects.get(id=article_id),
+                                               #'comments': Comments.objects.filter(comments_article_id=article_id),
+                                               })
+
